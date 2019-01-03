0af67e97557f908d38b437070e24cd908b11e2f0
jonbiro <jonnybrx@gmail.com>
2019-01-02 23:11:40 -0500

    A few visual tweaks and ActiveStorage set up for photos.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
 resources :movies do
-   resources :reviews# except: [:show, :index]
+   resources :reviews#,except: [:show, :index]
 end
 
 root 'movies#index'
