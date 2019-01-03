7c8daf483d61424927aa80d0936de9c6a8c00333
jonbiro <jonnybrx@gmail.com>
2019-01-02 22:41:18 -0500

    Mostly working

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,86 @@
+class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
+
+  # GET /movies
+  # GET /movies.json
+  def index
+    @movies = Movie.all
+    # @movie = current_user.movies.build
+
+  end
+
+  # GET /movies/1
+  # GET /movies/1.json
+  def show
+    # @reviews = Review.where(movie_id: @movie.id).order("created_at DESC")
+    @reviews = @movie.reviews.order("created_at DESC")
+    @current_user = current_user
+  end
+
+  # GET /movies/new
+  def new
+    @movie = current_user.movies.build
+  end
+
+  # GET /movies/1/edit
+  def edit
+
+  end
+
+  # POST /movies
+  # POST /movies.json
+  def create
+    @movie = current_user.movies.build(movie_params)
+
+    @movie.image.attach(movie_params['image'])
+
+    respond_to do |format|
+      if @movie.save
+        format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
+        format.json { render :show, status: :created, location: @movie }
+      else
+        format.html { render :new }
+        format.json { render json: @movie.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /movies/1
+  # PATCH/PUT /movies/1.json
+  def update
+
+    respond_to do |format|
+      if @movie.update(movie_params)
+        format.html { redirect_to @movie, notice: 'Movie was successfully updated.' }
+        format.json { render :show, status: :ok, location: @movie }
+      else
+        format.html { render :edit }
+        format.json { render json: @movie.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /movies/1
+  # DELETE /movies/1.json
+  def destroy
+    @movie.destroy
+    respond_to do |format|
+      format.html { redirect_to movies_url, notice: 'Movie was successfully deleted.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def movie_params
+      params.require(:movie).require(:title)
+      params.require(:movie).permit(:title, :description, :movie_length, :director, :rating, :image, :image_url)
+    end
+end
---
+++ app/models/movie.rb
@@ -0,0 +1,5 @@
+class Movie < ApplicationRecord
+  belongs_to :user
+  has_many :reviews
+  has_one_attached :image
+end
